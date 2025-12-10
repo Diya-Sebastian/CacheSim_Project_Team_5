f248190e412987874b39cf3fec26877f9788031e
DiyaSebastian <diyasebastian2003@gmail.com>
2025-12-10 23:20:07 +0530

    Added FIFO module initial structure

---
+++ replacement/fifo.py
@@ -0,0 +1,10 @@
+class FIFOCache:
+    def __init__(self, size):
+        self.size = size
+        self.cache = []
+
+    def access(self, block):
+        pass  # logic to be added later
+
+    def display(self):
+        return self.cache
