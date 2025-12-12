d00b659f08903489c7beb6a7ffc6865159ce49b6
biya <biyajomon966@gmail.com>
2025-12-12 23:05:31 +0530

    Implemented LFU access logic

--- replacement/lfu.py
+++ replacement/lfu.py
@@ -2,7 +2,35 @@ class LFUCache:
     def __init__(self, size):
         self.size = size
         self.cache = {}        
-        self.order = []        
+        self.order = []     
+
+    def access(self, block):
+        def access(self, block):
+    hit = block in self.cache
+
+    if hit:
+        self.cache[block] += 1
+        print(f"Accessed: {block} | HIT  | Freq: {self.cache[block]}")
+    else:
+        if len(self.cache) >= self.size:
+            min_freq = min(self.cache.values())
+            candidates = [b for b in self.cache if self.cache[b] == min_freq]
+
+            for b in self.order:
+                if b in candidates:
+                    evict = b
+                    break
+
+            print(f"Evicted: {evict}")
+            self.cache.pop(evict)
+            self.order.remove(evict)
+
+        self.cache[block] = 1
+        self.order.append(block)
+        print(f"Accessed: {block} | MISS | Cache: {self.cache}")
+
+    return hit
+   
 
     def display(self):
         return self.cache
