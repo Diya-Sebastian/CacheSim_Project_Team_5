68b96cd5270df0bbe3b817291572fff71c3fbd75
DarilTom <dariltom775@gmail.com>
2025-12-14 21:31:04 +0530

    implemented cache hit or miss logic

--- mapping/direct.py
+++ mapping/direct.py
@@ -9,3 +9,10 @@ class DirectMappingCache:
     def access_memory(self, block_number):
         # Direct mapping formula
         line = block_number % self.cache_lines
+        
+         # Check for hit or miss
+        if self.cache[line] == block_number:
+            return f"Cache HIT: Block {block_number} found in line {line}"
+        else:
+            self.cache[line] = block_number
+            return f"Cache MISS: Block {block_number} loaded into line {line}"
