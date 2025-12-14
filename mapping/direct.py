23c447edef6ce8d1be60c427e2ddf46601eca77f
DarilTom <dariltom775@gmail.com>
2025-12-14 21:29:17 +0530

    added accces_memory function

--- mapping/direct.py
+++ mapping/direct.py
@@ -5,3 +5,7 @@ class DirectMappingCache:
     def __init__(self, cache_lines):
         self.cache_lines = cache_lines
         self.cache = [None] * cache_lines
+    
+    def access_memory(self, block_number):
+        # Direct mapping formula
+        line = block_number % self.cache_lines
